a0e389b10aa6819d28abac751c82db2eed8c103d
kumodiva <kumodiva@gmail.com>
2022-09-17 09:23:53 +0800

    login screen

---
+++ screens/AuthScreen.js
@@ -0,0 +1,113 @@
+import {
+    ActivityIndicator,
+    Keyboard,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+  } from "react-native";
+  import React, { useState } from "react";
+  import { useNavigation } from "@react-navigation/native";
+  import axios from "axios";
+  import AsyncStorage from "@react-native-async-storage/async-storage";
+  import { PROFILE_SCREEN, API, API_LOGIN } from "../constants";
+  
+  export default function AuthScreen() {
+    const navigation = useNavigation();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorText, setErrorText] = useState("");
+    const [loading, setLoading] = useState(false);
+  
+    async function login() {
+      setLoading(true);
+      Keyboard.dismiss();
+      try {
+        const response = await axios.post(API + API_LOGIN, {
+          username,
+          password,
+        });
+        await AsyncStorage.setItem("token", response.data.access_token);
+        navigation.navigate(PROFILE_SCREEN);
+      } catch (error) {
+        console.log(error.response);
+        setErrorText(error.response.data.description);
+      }
+      setLoading(false);
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>THE PRATA COUNCIL</Text>
+  
+        <TextInput
+          style={styles.inputView}
+          placeholder="Email"
+          value={username}
+          onChangeText={(username) => setUsername(username)}
+        />
+  
+        <TextInput
+          style={styles.inputView}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(pw) => setPassword(pw)}
+        />
+  
+        <TouchableOpacity
+          style={styles.button}
+          onPress={async () => {
+            await login();
+          }}
+        >
+          {loading ? (
+            <ActivityIndicator style={styles.buttonText} />
+          ) : (
+            <Text style={styles.buttonText}>LOGIN</Text>
+          )}
+        </TouchableOpacity>
+  
+        <Text style={styles.errorText}>{errorText}</Text>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    errorText: {
+      marginTop: 20,
+      fontSize: 15,
+      color: "red",
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      paddingTop: 100,
+      padding: 25,
+    },
+    title: {
+      fontWeight: "bold",
+      textAlign: "center",
+      color: "#6D3D78",
+      fontSize: 40,
+      marginBottom: 50,
+    },
+    inputView: {
+      backgroundColor: "#F1F0F5",
+      borderRadius: 5,
+      marginBottom: 30,
+      padding: 20,
+    },
+    button: {
+      backgroundColor: "#C68BD4",
+      borderRadius: 15,
+      width: "100%",
+    },
+    buttonText: {
+      textAlign: "center",
+      fontWeight: "600",
+      fontSize: 28,
+      padding: 20,
+      color: "white",
+    },
+  });
